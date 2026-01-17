a2a9c85a1b7ab51cd83b138cc5de81b8d9170283
Ellyria34 <leon.sarah24@gmail.com>
2026-01-17 23:37:29 +0100

    test: refactor intent analyzer tests using theory

--- tests/AssistantIT.Console.Tests/Intent/SimpleIntentAnalyserTests.cs
+++ tests/AssistantIT.Console.Tests/Intent/SimpleIntentAnalyserTests.cs
@@ -7,32 +7,19 @@ namespace AssistantIT.Console.Tests.Intent
 {
     public class SimpleIntentAnalyzerTests
     {
-        [Fact]
-        public void Analyze_WhenInputContainsLog_ReturnsAnalyzeLogs()
-        {
-            // Arrange
-            var Analyzer = new SimpleIntentAnalyzer();
-            var userInput = "Erreur détectéé dans les logs de production";
-            
-            // Act
-            var result = Analyzer.Analyze(userInput);
-        
-            // Assert
-            Assert.Equal(UserIntent.AnalyzeLogs, result);
-        }
-
-        [Fact]
-        public void Analyze_WhenInputContainTicket_ReturnAnalyzeTicket()
+        [Theory]
+        [InlineData("Erreur détectée dans les logs de production", UserIntent.AnalyzeLogs)]
+        [InlineData("Voici un ticket qui est remonté.", UserIntent.AnalyzeTicket)]
+        public void Analyze_WhenInputContainsKeyword_ReturnsExpectedIntent(string userInput, UserIntent expectedIntent)
         {
             //Arrange
             var analyzer = new SimpleIntentAnalyzer();
-            var userInput = "Voici un ticket qui est remonté.";
 
             //Act
             var result = analyzer.Analyze(userInput);
 
             //Assert
-            Assert.Equal(UserIntent.AnalyzeTicket, result);
+            Assert.Equal(expectedIntent, result);
         }
     }
 }
